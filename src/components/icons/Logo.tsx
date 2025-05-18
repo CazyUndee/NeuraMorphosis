a967b2411cf66563a790a0d53ab5b3e3dec60e07
Rooney <cazyundee@gmail.com>
2025-05-18 19:55:10 +0000

    also bruh what is that logo for neuramorphosis??? pls make it better lol what is that even a house with arrows?

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -12,7 +12,8 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
         className="mr-2 text-primary"
         {...props}
       >
-        <path d="M16 2c-1.7 0-3.2.8-4.2 2L4 10.2V20h2v-9l7.8-7.8c.5-.4 1.1-.6 1.8-.6s1.3.2 1.8.6L26 11v9h2V10.2L20.2 4c-1-1.2-2.5-2-4.2-2zm0 4.5L6.5 14H10v12h2V14h2v12h2V14h2v12h2V14h3.5L16 6.5zM2 22v8h28v-8H2zm2 2h24v4H4v-4z" />
+        {/* New abstract logo design */}
+        <path d="M6,26 L10,26 L14,6 L10,6 Z M10,6 L14,6 L22,26 L18,26 Z M18,26 L22,26 L26,6 L22,6 Z" />
       </svg>
       <span className="text-2xl font-bold text-foreground">
         Neura<span className="text-primary">Morphosis</span>
